eae0d7684a777400758580140e79afcd52f3ddbf
unknown <lopez.thinktank@gmail.com>
2022-08-07 14:57:02 -0700

    updated most files with various changes, decided to use a whiteboard markdown file from now on. Used bootstrap and modified code for 1 hours timeblocks with modifications.

--- Develop/Develop/script.js
+++ Develop/Develop/script.js
@@ -1,5 +1,47 @@
-// var today = moment();
-// $("#timeHeader").text(today.format("MMM Do, YYYY"));
+//Started with the most current date and time
 
-var weekDay = moment("1-1-2022", "M-D-YYYY").format("ddd MMM Do, YYYY");
-$("#timeHeader").text(weekDay);
+var past = [];
+var present = [];
+var future = [];
+
+//The jquery elements that can be used like this code below
+// var clock = document.getElementById("timeHeader");
+
+
+var currentDay = new Date();
+
+var present = moment("08-06-2022", "M-D-YYYY").format("MMMM Do YYYY, h:mm:ss", true);
+$("#currentDay").text(currentDay);
+console.log(currentDay);
+
+var events = localStorage.getItem('saveBtn');
+
+// function now() {
+//     console.log(clock);
+// };
+
+//The click event below is being used to change the color of the timeblock when clicked.
+document.getElementById("#b1").addEventListener("click", changeColor, true);
+
+function changeColor(present) {
+    document.syle = present;
+  
+};
+
+
+//Using this button to save to local storage
+// saveBtn.addEventListener("click", function() {
+//     if (events = true) {
+//         innerHTML.textContent = events;
+//       localStorage.setItem("events", );
+//     }
+//   });
+
+
+//each line will have to be saved independantly of the others. 
+// 9 hour workday - append new element  
+//classes <> to add color coding for hourly events
+//Use the css to reference the past present future 
+//use setInterval for timers and clear Interval for 
+// var weekDay = moment("1-1-2022", "M-D-YYYY").format("ddd MMM Do, YYYY");
+// $("#timeHeader").text(weekDay);
---
+++ Whiteboard.md
@@ -0,0 +1,21 @@
+# Work Day Scheduler
+
+## Main function Outline:
+    1. User pulls up page with current date
+    2. User can select a timeblock from 9am to 5pm for a schedule event. 
+    3. User will see color coded 1 hour time slots that represent past, present, and future.
+    4. User can input text directly to page's timeblock.
+    5. User will hit save button to store entry into local storage for recall. 
+    6. After hitting the refresh button, my events will remain.
+
+
+## Coding Logic Outline: 
+
+1. Use moment api to show current time and date in Header. Link to the individual time coded functions below.
+2. Create x9 class timeblock elements(bootstrap?) that can be updated in real time and stored in local storage.
+3. Use a function to link the saved button to all timeblocks. 
+4. Use a function to color code all the time slots for past. Use the existing css selectors.  
+5. Use function to color code the timeblock for present. Use css selectors already designated for present time. 
+6. Use function to color code for future time. Use css selectors designated for the future timeblock. 
+
+
